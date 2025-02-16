aefe72f9e01dbbaa1247556a52d2c11855daee2a
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2025-02-16 14:08:08 +0200

    finish server

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv'
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Middleware
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
